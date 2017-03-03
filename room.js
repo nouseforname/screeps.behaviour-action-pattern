0691804a8c9ce2278b40b807203c7d1c7e8f0680
Dan (Asku) <askewholdings@gmail.com>
2017-03-03 12:01:18 +0000

    typo

--- room.js
+++ room.js
@@ -1049,7 +1049,7 @@ mod.extend = function(){
     Room.prototype.checkPowerBank = function() {
     if (!this.powerBank) return; // no power bank in room
     	//power > 2500, and ticksToDecay > 4500
-        const currentFlags = FlagDir.count(FLAG_COLOR.invade.powerMining, this.powweBank.pos, false);
+        const currentFlags = FlagDir.count(FLAG_COLOR.invade.powerMining, this.powerBank.pos, false);
     	const flagged = FlagDir.find(FLAG_COLOR.invade.powerMining, this.powerBank.pos, true);
     	if(!flagged && currentFlags < MAX_AUTO_POWER_MINING_FLAGS){
     	    if(this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
