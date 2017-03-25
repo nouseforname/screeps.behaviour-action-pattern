1879e279cbe6711c6235994d2add42bed5064c3f
Spedwards <improbablepuzzle@gmail.com>
2017-03-26 01:06:32 +1100

    Doesn't belong

--- room.js
+++ room.js
@@ -1182,18 +1182,6 @@ mod.extend = function(){
             this.roadConstructionTrace[key] = 1;
         else this.roadConstructionTrace[key]++;
     };
-    Room.prototype.checkPowerBank = function() {
-    if (!this.powerBank) return; // no power bank in room
-    	//power > 2500, and ticksToDecay > 4500
-        const currentFlags = FlagDir.count(FLAG_COLOR.invade.powerMining, this.powerBank.pos, false);
-    	const flagged = FlagDir.find(FLAG_COLOR.invade.powerMining, this.powerBank.pos, true);
-    	if(!flagged && currentFlags < MAX_AUTO_POWER_MINING_FLAGS){
-    	    if(this.powerBank.power > 2500 && this.powerBank.ticksToDecay > 4500){
-    		    // Place a flag
-    		    this.createFlag(this.powerBank.pos, null, FLAG_COLOR.invade.powerMining.color, FLAG_COLOR.invade.powerMining.secondaryColor);
-    	    }
-        }
-    };
     Room.prototype.saveTowers = function(){
         let towers = this.find(FIND_MY_STRUCTURES, {
             filter: {structureType: STRUCTURE_TOWER}
